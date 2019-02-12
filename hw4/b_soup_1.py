cbfa541d385182525eaef456dfa90da9b7643c22
bjrhindress <bjrhindress@gmail.com>
2019-02-12 00:37:48 -0500

    Update b_soup_1.py

--- hw4/b_soup_1.py
+++ hw4/b_soup_1.py
@@ -7,55 +7,6 @@ html = urlopen('https://www.treasury.gov/resource-center/'
 
 bsyc = BeautifulSoup(html.read(), "lxml")
 
-# fout = open('bsyc_temp.txt', 'wt',
-# 		encoding='utf-8')
-
-# fout.write(str(bsyc))
-
-# fout.close()
-
-# # print the first table
-# print(str(bsyc.table))
-# # ... not the one we want
-
-# so get a list of all table tags
-# table_list = bsyc.findAll('table')
-
-# how many are there?
-# print('there are', len(table_list), 'table tags')
-
-# # look at the first 50 chars of each table
-# for t in table_list:
-#     print(str(t)[:50])
-
-# only one class="t-chart" table, so add that
-# to findAll as a dictionary attribute
-# tc_table_list = bsyc.findAll('table',
-#                       { "class" : "t-chart" } )
-
-# # how many are there?
-# print(len(tc_table_list), 't-chart tables')
-
-# # only 1 t-chart table, so grab it
-# tc_table = tc_table_list[0]
-
-# # what are this table's components/children?
-# for c in tc_table.children:
-#     print(str(c)[:50])
-
-# # tag tr means table row, containing table data
-# # what are the children of those rows?
-# # for c in tc_table.children:
-# #     for r in c.children:
-# #         print(str(r)[:50])
-
-# # # we have found the table data!
-# # # just get the contents of each cell
-# # for c in tc_table.children:
-# #     for r in c.children:
-# #         print(r.contents)
-
-
 # only one class="t-chart" table, so add that
 # to findAll as a dictionary attribute
 t_chart_tables = bsyc.findAll('table', { "class" : "t-chart" } )
@@ -101,17 +52,12 @@ with open('daily_yield_curves.txt','w') as output:
     ### Table 1 output (B-records) ###
     column_width = 11
     column_label_1 = daily_yield_curves[0]
-    #column_label_2 = ['Code', 'Month', 'Type', 'Exp Date', 'Code', 'Exp Date']
     column_label_3 = '-------'
 
     # print header
     for column_label in column_label_1:
         output.write(column_label.ljust(column_width))
     output.write('\n')
-
-    # for column_label in column_label_2:
-    #     output.write(column_label.ljust(column_width))
-    # output.write('\n')
 
     for i in range(0,len(daily_yield_curves[0]),1):
         output.write(column_label_3.ljust(column_width))
@@ -138,47 +84,44 @@ import numpy as np
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-# Make data.
-#X = days since 1/02/18 
 import datetime 
 start_date = datetime.datetime(2018, 1, 2)
 
-X = np.append([[]], [[]], axis = 0)
-
+first = True
 for row in daily_yield_curves[1:]:
+	
+	# Finding dist from first /1/02/18
 	row_date_list = row[0].split('/')
-	print(row_date_list)
 	row_date = datetime.datetime(int('20'+row_date_list[2]),
 		int(row_date_list[0]), int(row_date_list[1]))	
 	days_diff = row_date - start_date
-	#print(np.repeat(days_diff.days,len(row[1:])))
-	if len(X) < 1:
+
+	if first:
 		X = np.repeat(days_diff.days,len(row[1:]))
-	else:
-		X = np.append(X,np.repeat(days_diff.days,len(row[1:])), axis = 0)
-#np.append([[1,1],[2,2]],[[3,3]], axis = 0)
+		Y = np.array([1, 3, 6, 12, 24, 36, 60, 84, 120, 240, 360])
+		Z = np.array(row[1:])
+		first = False
+	else: 
+		X = np.vstack((X, np.repeat(days_diff.days,len(row[1:]))))
+		Y = np.vstack((Y,[1, 3, 6, 12, 24, 36, 60, 84, 120, 240, 360]))
+		Z = np.vstack((Z,np.array(row[1:])))
 
 print(X)
+print(Y)
+print(Z)
 
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+# Customize the z axis.
+ax.set_xlim(0, 400)
+ax.set_ylim(0, 400)
+ax.set_zlim(0, 4)
 
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
-# X = np.arange()
-# X = np.arange(-5, 5, 0.25)
-# Y = np.arange(-5, 5, 0.25)
-# X, Y = np.meshgrid(X, Y)
-# R = np.sqrt(X**2 + Y**2)
-# Z = np.sin(R)
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
 
-# # Plot the surface.
-# surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-#                        linewidth=0, antialiased=False)
-
-# # Customize the z axis.
-# ax.set_zlim(-1.01, 1.01)
-# ax.zaxis.set_major_locator(LinearLocator(10))
-# ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-# # Add a color bar which maps values to colors.
-# fig.colorbar(surf, shrink=0.5, aspect=5)
-
-# plt.show()
+plt.show()
